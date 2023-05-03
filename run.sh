684dfe10adea8ed33cc5a7937a671198ae280d51
MiniBossGPT <132403495+MiniBossGPT@users.noreply.github.com>
2023-05-03 16:14:14 -0400

    [improv] prep for release (#3)
    Co-authored-by: John <minibossgpt@gmail.com>

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+python scripts/check_requirements.py requirements.txt
+if [ $? -eq 1 ]
+then
+    echo Installing missing packages...
+    pip install -r requirements.txt
+fi
+
+python scripts/check_requirements.py auto-gpt/requirements.txt
+if [ $? -eq 1 ]
+then
+    echo Installing missing packages...
+    pip install -r auto-gpt/requirements.txt
+fi
+python -m miniboss $@
+read -p "Press any key to continue..."
